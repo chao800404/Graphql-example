ac9df310aaf9a2d06f95bab6b53addcd1c24022e
chao <a0921342997@gmail.com>
2022-10-11 14:29:41 +0800

    Update comment

--- example-3/server/dist/resolver/Query/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Query = void 0;
-const auth_1 = require("./auth");
-const post_1 = require("./post");
-const profile_1 = require("./profile");
-exports.Query = Object.assign(Object.assign(Object.assign({}, post_1.postResolver), auth_1.userResolver), profile_1.profileResolver);
-//# sourceMappingURL=index.js.map
